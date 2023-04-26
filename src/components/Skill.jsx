2e256f7ef389967e715caa6558ccc381ac6ec84a
QuevDev <quevdev@outlook.com>
2023-04-25 20:39:41 -0400

    Skill icons are now interactable

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,11 +1,13 @@
 export default function Skill({ image = "", link = "", text = "" }) {
   return (
     <li className="text-cAccent" data-aos-once="true" data-aos="flip-down">
-      <img
-        className="bg-cBackground p-1 rounded-lg w-8 h-8 inline mr-1 relative bottom-1 drop-shadow-xl object-contain"
-        src={image}
-        alt=""
-      />
+      <a rel="noreferrer" target="_blank" href={link} className="skill-button">
+        <img
+          className="bg-cBackground p-1 rounded-lg w-8 h-8 inline mr-1 relative bottom-1 drop-shadow-xl object-contain"
+          src={image}
+          alt=""
+        />
+      </a>
       <a rel="noreferrer" target="_blank" href={link} className="inline ml-1">
         {text}
       </a>
