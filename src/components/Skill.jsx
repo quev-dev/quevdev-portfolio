76b46ce4b7dc26f37a083ebf811725bc2c7ec4b4
QuevDev <quevdev@outlook.com>
2023-04-25 20:26:59 -0400

    Updated "class" to "className" in skill component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,11 +2,11 @@ export default function Skill({ image = "", link = "", text = "" }) {
   return (
     <li className="text-cAccent" data-aos-once="true" data-aos="flip-down">
       <img
-        class="bg-cBackground p-1 rounded-lg w-8 h-8 inline mr-1 relative bottom-1 drop-shadow-xl object-contain"
+        className="bg-cBackground p-1 rounded-lg w-8 h-8 inline mr-1 relative bottom-1 drop-shadow-xl object-contain"
         src={image}
         alt=""
       />
-      <a rel="noreferrer" target="_blank" href={link} class="inline ml-1">
+      <a rel="noreferrer" target="_blank" href={link} className="inline ml-1">
         {text}
       </a>
     </li>
