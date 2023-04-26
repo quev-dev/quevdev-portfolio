ecd9fe6ff37e16f0004ab2718f438c4aa5f9bf4d
QuevDev <quevdev@outlook.com>
2023-04-25 20:25:41 -0400

    Made a skill component and added links to skills

---
+++ src/components/Skill.jsx
@@ -0,0 +1,14 @@
+export default function Skill({ image = "", link = "", text = "" }) {
+  return (
+    <li className="text-cAccent" data-aos-once="true" data-aos="flip-down">
+      <img
+        class="bg-cBackground p-1 rounded-lg w-8 h-8 inline mr-1 relative bottom-1 drop-shadow-xl object-contain"
+        src={image}
+        alt=""
+      />
+      <a rel="noreferrer" target="_blank" href={link} class="inline ml-1">
+        {text}
+      </a>
+    </li>
+  );
+}
