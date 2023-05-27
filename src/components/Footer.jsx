2a43c0e8e67d7e43c458dae94d5fb2360ef30afc
QuevDev <quevdev@outlook.com>
2023-05-27 11:47:26 -0400

    Adjusted footer layout & added EmailJS link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,79 +1,86 @@
-import svgBorder from "../content/svgs/border-footer.svg";
+import svgBorder from '../content/svgs/border-footer.svg';
 
 export default function Footer() {
   return (
     <footer>
-      <div className="animated-border-wrapper">
+      <div className='animated-border-wrapper'>
         <img
-          className="animated-border shadow-xl"
+          className='animated-border shadow-xl'
           src={svgBorder}
-          alt="Border Image"
+          alt='Border Image'
         />
       </div>
-      <section className="bg-cAccent text-cBackground p-4 flex flex-col md:flex-row md:items-center md:justify-center">
-        <article className="p-4">
-          <h4 className="font-bold">&copy; QuevDev 2023</h4>
+      <section className='bg-cAccent text-cBackground p-4 flex flex-col md:flex-row md:justify-center'>
+        <article className='p-4 pb-0'>
+          <h4 className='font-bold'>&copy; QuevDev 2023</h4>
           <p>All rights reserved.</p>
         </article>
-        <article className="p-4">
-          <div className="mb-4">
-            <h4 className="font-bold">Resources</h4>
+        <article className='p-4'>
+          <div className='mb-4'>
+            <h4 className='font-bold'>Resources</h4>
             <p>
               You can view the resources I used for building this portfolio
               through the links below.
             </p>
           </div>
-          <ul className="flex flex-wrap gap-4">
+          <ul className='flex flex-wrap gap-4 md:gap-8'>
             <div>
-              <h6 className="text-sm font-bold">Technologies</h6>
-              <li className="flex flex-col">
-                <a rel="noreferrer" target="_blank" href="https://astro.build/">
+              <h6 className='text-sm font-bold'>Technologies</h6>
+              <li className='flex flex-col'>
+                <a rel='noreferrer' target='_blank' href='https://astro.build/'>
                   Astro
                 </a>
                 <a
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiAk4vwspv-AhXkF1kFHQpLBkkQFnoECAkQAQ&url=https%3A%2F%2Freact.dev%2F&usg=AOvVaw1tEjYYiD7LQlxO53dgjTHV"
+                  rel='noreferrer'
+                  target='_blank'
+                  href='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiAk4vwspv-AhXkF1kFHQpLBkkQFnoECAkQAQ&url=https%3A%2F%2Freact.dev%2F&usg=AOvVaw1tEjYYiD7LQlxO53dgjTHV'
                 >
                   React
                 </a>
                 <a
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://tailwindcss.com/"
+                  rel='noreferrer'
+                  target='_blank'
+                  href='https://tailwindcss.com/'
                 >
                   Tailwind
                 </a>
               </li>
             </div>
             <div>
-              <h6 className="text-sm font-bold">Other</h6>
-              <li className="flex flex-col">
+              <h6 className='text-sm font-bold'>Other</h6>
+              <li className='flex flex-col'>
                 <a
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://www.netlify.com/"
+                  rel='noreferrer'
+                  target='_blank'
+                  href='https://www.netlify.com/'
                 >
                   Netlify
                 </a>
                 <a
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://icons.getbootstrap.com/"
+                  rel='noreferrer'
+                  target='_blank'
+                  href='https://icons.getbootstrap.com/'
                 >
                   Bootstrap Icons
                 </a>
               </li>
             </div>
             <div>
-              <h6 className="text-sm font-bold">Libraries</h6>
-              <li className="flex flex-col">
+              <h6 className='text-sm font-bold'>Libraries</h6>
+              <li className='flex flex-col'>
                 <a
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://michalsnik.github.io/aos/"
+                  rel='noreferrer'
+                  target='_blank'
+                  href='https://michalsnik.github.io/aos/'
                 >
                   AOS
+                </a>
+                <a
+                  rel='noreferrer'
+                  target='_blank'
+                  href='https://www.emailjs.com/'
+                >
+                  EmailJS
                 </a>
               </li>
             </div>
